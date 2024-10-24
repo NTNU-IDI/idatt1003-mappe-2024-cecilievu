faba2ceafc5d82dd0047e6b0f66bb6ca1275f762
cecilievu <cecilie_vu@hotmail.com>
2024-10-24 13:29:38 +0200

    made some modifications

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -1,6 +1,7 @@
-package edu.ntnu.idi.idatt;
+package main.java.edu.ntnu.idi.idatt;
 
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
 }
